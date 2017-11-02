8ca92a444454dc9f0a8ed034aa115565cdf3aa59
Oguz Gelal <oguz.gelal@vub.ac.be>
2017-11-02 16:31:37 +0100

    proptypes fix

--- src/components/landing-page/LandingPage.js
+++ src/components/landing-page/LandingPage.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import Clouds from '../common/clouds/Clouds';
 import * as Icon from 'react-feather';
 
