4a5a977a10c092da500c01041636c6425c4ad919
Oguz Gelal <oguz.gelal@vub.ac.be>
2017-11-04 14:42:54 +0100

    avoid clouds component to rerender

--- src/components/common/clouds/Clouds.js
+++ src/components/common/clouds/Clouds.js
@@ -4,45 +4,61 @@ import PropTypes from 'prop-types';
 import Cloud from './Cloud';
 import './Clouds.scss';
 
-const rand = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
+class Clouds extends React.Component {
+  constructor(props, context) {
+    super(props, context);
 
-const Clouds = (props) => {
-  let count = props.count || 30;
-  let speedMax = props.speedMax || 50, speedMin = props.speedMin || 20;
-  let sizeMax = props.sizeMax || 80, sizeMin = props.sizeMin || 10;
-  let clouds = [];
-  for (let i = 0; i < count; i++) {
-    let speed = rand(speedMin, speedMax);
-    let size = rand(sizeMin, sizeMax) / 100;
-    let top = rand(0, 80);
-    let delay = rand(-50, 50);
-    let styles = {
-      'WebkitAnimation': `clouds--animate ${speed}s linear infinite`,
-      'MozAnimation': `clouds--animate ${speed}s linear infinite`,
-      'animation': `clouds--animate ${speed}s linear infinite`,
-      'WebkitAnimationDelay': `${delay}s`,
-      'MozAnimationDelay': `${delay}s`,
-      'animationDelay': `${delay}s`,
-      'WebkitTransform': `scale(${size})`,
-      'MozTransform': `scale(${size})`,
-      'transform': `scale(${size})`,
-      'top': `${top}vh`
+    this.state = {
     };
-    clouds.push(
-      <div className="clouds--wrapper" key={'cloud' + i} style={styles}>
-        <Cloud />
+  }
+
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  rand(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  render() {
+    const self = this;
+    let count = self.props.count || 30;
+    let speedMax = self.props.speedMax || 50;
+    let speedMin = self.props.speedMin || 20;
+    let sizeMax = self.props.sizeMax || 80;
+    let sizeMin = self.props.sizeMin || 10;
+    let clouds = [];
+    for (let i = 0; i < count; i++) {
+      let speed = self.rand(speedMin, speedMax);
+      let size = self.rand(sizeMin, sizeMax) / 100;
+      let top = self.rand(0, 80);
+      let delay = self.rand(-50, 50);
+      let styles = {
+        'WebkitAnimation': `clouds--animate ${speed}s linear infinite`,
+        'MozAnimation': `clouds--animate ${speed}s linear infinite`,
+        'animation': `clouds--animate ${speed}s linear infinite`,
+        'WebkitAnimationDelay': `${delay}s`,
+        'MozAnimationDelay': `${delay}s`,
+        'animationDelay': `${delay}s`,
+        'WebkitTransform': `scale(${size})`,
+        'MozTransform': `scale(${size})`,
+        'transform': `scale(${size})`,
+        'top': `${top}vh`
+      };
+      clouds.push(
+        <div className="clouds--wrapper" key={'cloud' + i} style={styles}>
+          <Cloud />
+        </div>
+      );
+    }
+
+    return (
+      <div className="clouds--container">
+        {clouds}
       </div>
     );
   }
-
-  return (
-    <div className="clouds--container">
-      {clouds}
-    </div>
-  );
-};
+}
 
 Clouds.propTypes = {
   count: PropTypes.number,
