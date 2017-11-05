4701f2bfa626ac81aee8cbaa36563e2187d69bd1
Oguz Gelal <oguz.gelal@vub.ac.be>
2017-11-05 18:51:12 +0100

    better reactify fb login

--- src/utils/rxify.js
+++ src/utils/rxify.js
@@ -2,10 +2,12 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/bindCallback';
 
 export const fbLoginObservable = () => {
-  return Observable.bindCallback(window.FB.login, response => {
-    if (!response.authResponse) {
-      throw new Error('User cancelled login or did not fully authorize');
-    }
-    return response;
-  })();
+  return Observable.create(observer => {
+    if (!window.FB) { observer.error('Cannot load Facebook SDK') }
+    window.FB.login(res => {
+      if (!res.authResponse) { observer.error('User cancelled login or did not fully authorize'); }
+      observer.next(res)
+      observer.complete()
+    })
+  })
 }
