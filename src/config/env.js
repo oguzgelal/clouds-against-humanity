4c49f3cf6196de937b0ea892b5d22141bb1e15a6
Oguz Gelal <oguz.gelal@vub.ac.be>
2018-01-17 14:17:16 +0100

    set local url to backend deployed one

--- src/config/env.js
+++ src/config/env.js
@@ -1,8 +1,8 @@
 export default {
   FB_APP_ID: process.env.NODE_ENV === 'production' ? '151696645445865' : '2065138443715070',
   //LOBBY_SOURCE: process.env.NODE_ENV === 'production' ? 'wss://clouds-against-humanity.herokuapp.com:22759' : 'localhost:8080',
-  GAME_SOURCE: process.env.NODE_ENV === 'production' ? 'wss://clouds-against-humanity.herokuapp.com:3000' : 'localhost:3000',
-  LOBBY_SOURCE: 'https://cah-server-pephwyxeko.now.sh',
+  //GAME_SOURCE: process.env.NODE_ENV === 'production' ? 'wss://clouds-against-humanity.herokuapp.com:3000' : 'localhost:3000',
+  LOBBY_SOURCE: 'https://cah-server-wihowjrmkb.now.sh',
 
   CDN_JS: [
     'https://cdnjs.cloudflare.com/ajax/libs/izitoast/1.1.5/js/iziToast.min.js',
